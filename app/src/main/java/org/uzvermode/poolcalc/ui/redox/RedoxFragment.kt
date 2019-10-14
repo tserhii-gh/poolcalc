95574aa8fa4ecf0e448ac9095af7e488001f8afb
Serhii Tsynailo <serhii.tsynailo@gmail.com>
2019-10-14 13:18:17 +0300

    code cleanup, follow mvvp

--- app/src/main/java/org/uzvermode/poolcalc/ui/redox/RedoxFragment.kt
+++ app/src/main/java/org/uzvermode/poolcalc/ui/redox/RedoxFragment.kt
@@ -1,29 +1,19 @@
 package org.uzvermode.poolcalc.ui.redox
 
-import android.content.Context
-import android.graphics.Color
 import android.icu.text.DecimalFormat
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.NumberPicker
 import android.widget.SeekBar
 import android.widget.TextView
-import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
-import kotlinx.android.synthetic.main.fragment_redox.*
 import org.uzvermode.poolcalc.R
-import org.uzvermode.poolcalc.data.ClPhTable
 
 class RedoxFragment : Fragment() {
     private lateinit var redoxViewModel: RedoxViewModel
-    private var REDOX = 660
-    private var PH: Double = 7.4
-    private var clPhTable = ClPhTable()
 
 
     override fun onCreateView(
@@ -39,15 +29,19 @@ class RedoxFragment : Fragment() {
         val textPh: TextView = root.findViewById(R.id.text_ph)
         val textRedox: TextView = root.findViewById(R.id.text_redox)
         val textChlorine: TextView = root.findViewById(R.id.text_chlorine)
-        textChlorine.text = clPhTable.CLlevel(REDOX, PH)
         val phSeekBar: SeekBar = root.findViewById(R.id.ph_level)
         val rxSeekBar: SeekBar = root.findViewById(R.id.rx_level)
+        redoxViewModel.setPh(4)
+        redoxViewModel.setRedox(180)
 
         phSeekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
             override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
-                textPh.text = getConvertedPhValue(progress).toString()
-                textChlorine.text = clPhTable.CLlevel(REDOX, PH)
-
+                redoxViewModel.setPh(progress).observe(viewLifecycleOwner, Observer {
+                                        textPh.text = it
+                })
+                redoxViewModel.getActiveChlorine().observe(viewLifecycleOwner, Observer {
+                    textChlorine.text = it
+                })
             }
 
             override fun onStartTrackingTouch(seekBar: SeekBar) {
@@ -59,8 +53,12 @@ class RedoxFragment : Fragment() {
 
         rxSeekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
             override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
-                textRedox.text = getConvertedRxValue(progress).toString()
-                textChlorine.text = clPhTable.CLlevel(REDOX, PH)
+                redoxViewModel.setRedox(progress).observe(viewLifecycleOwner, Observer {
+                                        textRedox.text = it.toString()
+                })
+                redoxViewModel.getActiveChlorine().observe(viewLifecycleOwner, Observer {
+                    textChlorine.text = it
+                })
             }
 
             override fun onStartTrackingTouch(seekBar: SeekBar) {
@@ -69,40 +67,12 @@ class RedoxFragment : Fragment() {
             override fun onStopTrackingTouch(seekBar: SeekBar) {
             }
         })
-        textPh.text = getConvertedPhValue(phSeekBar.progress)
-//        context?.let {
-//            textPh.setShadowLayer(9.0f,0.0f,0.0f, ContextCompat.getColor(it ,R.color.colorPh))
-//            textRedox.setShadowLayer(9.0f,0.0f,0.0f, ContextCompat.getColor(it ,R.color.colorRx))
-//            textChlorine.setShadowLayer(9.0f,0.0f,0.0f, ContextCompat.getColor(it ,R.color.colorCl))
-//            textPh.setShadowLayer(9.0f,0.0f,0.0f, ContextCompat.getColor(it ,R.color.colorCl))
-//        }
-//        textPh.setShadowLayer(10.0f,5.0f,5.0f, Color.BLUE)
-        textRedox.text = getConvertedRxValue(rxSeekBar.progress)
+        textPh.text = redoxViewModel.getPh()
+        textRedox.text = redoxViewModel.getRedox()
 
-        redoxViewModel.text.observe(this, Observer {
-//            textView.text = it
-            Log.e("ViewModel", it)
+        redoxViewModel.getActiveChlorine().observe(this, Observer {
+                        textChlorine.text = it
         })
-
         return root
-    }
-
-    private fun getConvertedPhValue(intVal: Int): String {
-        val f: DecimalFormat = DecimalFormat("#.#")
-        val pHValue: Double = 6.9 + 0.1 * intVal;
-//        Log.e("pH Value", f.format(pHValue))
-        if (pHValue == 7.1) {
-            PH = 7.2
-            return "7.2"
-
-        } else {
-            PH = f.format(pHValue).toDouble()
-            return f.format(pHValue)
-        }
-    }
-
-    private fun getConvertedRxValue(intVal: Int): String {
-        REDOX = 480 + intVal
-        return REDOX.toString()
     }
 }
--- app/src/main/java/org/uzvermode/poolcalc/ui/redox/RedoxViewModel.kt
+++ app/src/main/java/org/uzvermode/poolcalc/ui/redox/RedoxViewModel.kt
@@ -1,22 +1,115 @@
 package org.uzvermode.poolcalc.ui.redox
 
 import android.util.Log
-import android.widget.SeekBar
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import java.text.DecimalFormat
 
 class RedoxViewModel : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is Redox Fragment"
-    }
-    val text: LiveData<String> = _text
+    private val TAG = "RedoxViewModel"
 
 
-    private fun getConvertedValue(intVal :Int ): Float {
-        var floatValue: Float = 0.0F;
-        floatValue = .5F * intVal;
-        return floatValue;
+
+    private val chlorine = doubleArrayOf(
+        0.2,
+        0.3,
+        0.4,
+        0.5,
+        0.6,
+        0.65,
+        0.67,
+        0.75,
+        0.8,
+        0.85,
+        0.9,
+        0.95,
+        1.0,
+        1.1,
+        1.2,
+        1.3,
+        1.4,
+        1.5,
+        1.7,
+        1.8,
+        2.0,
+        2.2,
+        2.5
+    )
+//    private val phLevel = doubleArrayOf(6.9, 7.0, 7.2, 7.3, 7.4, 7.5, 7.6, 7.7, 7.8, 7.9, 8.0, 8.1, 8.2, 8.3)
+    private val phLv = arrayOf("6.9", "7.0", "7.2", "7.3", "7.4", "7.5", "7.6", "7.7", "7.8", "7.9", "8.0", "8.1", "8.2", "8.3")
+
+    private val table: Array<IntArray> = arrayOf(
+        intArrayOf(507,561,599,629,652,663,673,682,690,698,706,713,720,733,744,755,765,774,790,798,812,824,841), // 6.9
+        intArrayOf(505,558,596,625,648,658,669,677,686,694,702,708,715,727,739,749,759,768,784,792,805,818,834), // 7.0
+        intArrayOf(502,553,590,618,640,650,660,668,677,684,692,698,705,717,728,738,747,756,771,779,792,804,826), // 7.2
+        intArrayOf(500,550,586,615,637,646,656,664,672,680,687,694,700,712,722,732,742,750,765,773,785,797,813), // 7.3
+        intArrayOf(499,548,583,611,632,642,651,660,668,675,682,689,695,707,717,727,736,744,759,766,779,791,806), // 7.4
+        intArrayOf(497,546,580,607,629,638,647,655,663,670,677,684,690,701,712,721,730,738,753,760,773,784,800), // 7.5
+        intArrayOf(496,544,577,604,625,634,643,651,659,666,673,679,685,696,706,716,724,732,747,754,766,777,792), // 7.6
+        intArrayOf(494,541,574,600,621,630,639,647,654,661,668,674,680,691,701,710,719,727,741,748,760,771,785), // 7.7
+        intArrayOf(493,539,571,597,617,626,635,642,650,657,663,669,675,696,695,705,713,721,735,741,753,764,778), // 7.8
+        intArrayOf(491,536,568,593,613,622,630,638,645,652,658,664,670,680,690,699,707,715,728,735,747,757,771), // 7.9
+        intArrayOf(490,534,565,590,610,618,626,634,641,647,654,659,665,675,685,694,702,709,722,729,740,751,764), // 8.0
+        intArrayOf(488,532,562,586,605,614,622,629,636,643,649,654,660,670,679,688,696,703,716,722,734,744,757), // 8.1
+        intArrayOf(487,529,559,583,602,610,618,625,632,638,644,650,655,665,674,682,690,697,710,716,727,737,751), // 8.2
+        intArrayOf(486,527,556,580,598,606,614,620,627,633,639,645,650,660,669,677,684,691,704,710,721,730,744) // 8.3
+    )
+
+    private val resultPh = MutableLiveData<String>()
+    private val resultRedox = MutableLiveData<Int>()
+    private val resultChlorine = MutableLiveData<String>()
+    private val maxRedox = MutableLiveData<Int>()
+    private var lastValue = "---"
+
+
+    fun getActiveChlorine(): LiveData<String> {
+        val REDOX = resultRedox.value
+        val PH = resultPh.value
+
+        val redoxRow = table[phLv.indexOf(PH)]
+        maxRedox.value = redoxRow.last() - 481
+        Log.e(TAG, "$PH - $REDOX - ${maxRedox.value}")
+        if (redoxRow.contains(REDOX!!)){
+            resultChlorine.value = chlorine[redoxRow.indexOf(REDOX)].toString()
+        } else {
+            try {
+                val rx = redoxRow[-(redoxRow.binarySearch(REDOX) + 1)]
+                lastValue = chlorine[table[phLv.indexOf(PH)].indexOf(rx)].toString()
+                resultChlorine.value = lastValue
+            } catch (e: ArrayIndexOutOfBoundsException) {
+                resultChlorine.value = lastValue
+            }
+        }
+        return resultChlorine
     }
+
+    fun getPh(): String? {
+        return resultPh.value
+    }
+
+
+    fun setPh(intVal: Int):LiveData<String> {
+        resultPh.value = phLv[intVal]
+        Log.e(TAG, resultPh.value!!)
+        return resultPh
+    }
+
+
+    fun setRedox(intVal: Int): LiveData<Int> {
+        resultRedox.value = (480 + intVal)
+        return resultRedox
+    }
+
+
+    fun getRedox(): String {
+        return resultRedox.value.toString()
+    }
+
+
+    fun getMaxRedox(): Int {
+        return maxRedox.value!!
+    }
+
 }
